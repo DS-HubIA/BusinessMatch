8fc20ed06c63c9af90e004e1a97ae6ae437517b1
DS-HubIA <dsmorigo@gmail.com>
2025-10-12 20:32:52 -0300

    feat: Add static files (CSS, JS)

---
+++ app/static/js/app.js
@@ -0,0 +1,8 @@
+// Business Match SEBRAE - JavaScript
+console.log('Business Match SEBRAE loaded!');
+
+// Funções para swipe
+function handleSwipe(direction, businessId) {
+    console.log('Swiped', direction, 'on business', businessId);
+    // Implementar swipe real depois
+}
